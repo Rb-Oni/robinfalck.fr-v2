6437f24f52e9ccd27b818f481edef4ed347a8d23
Rb-Oni <robin.falck57@gmail.com>
2023-02-21 21:17:39 +0100

    home page project section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,14 @@ module.exports = {
 				'black': '#191919',
 			},
 			height: {
-				'50vh': '50vh'
+				'50vh': '50vh',
+				'75vh': '75vh'
 			},
+			backgroundImage: {
+				'hokusai': "url('/public/img/hokusai.png')",
+				'leslutinstournes': "url('/public/img/leslutinstournes.png')",
+				'mltoiturecreation': "url('/public/img/mltoiturecreation.png')"
+			}
 		},
 	},
 	plugins: [],
